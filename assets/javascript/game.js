8dc63a24a8a7c7bbce6da0f32fa8d1a0a3e1fea7
skeown13 <code@keown.net>
2020-04-21 20:03:33 -0700

    psuedocoded my javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// Create an array of all possible choices
+
+// Randomly choose one choice
+
+// Have user make a guess
+
+// If guess is correct update Wins
+
+// If guess is incorrect change guesses Left to -1 and add user's guess to an array
+
+// When guesses left hits 0, update Losses
